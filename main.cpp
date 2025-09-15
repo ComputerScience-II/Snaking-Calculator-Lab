2ddc839281d834c651cabe559dcf71f95c5db743
AdityaP <pathak.aditya.in@gmail.com>
2025-09-14 22:33:46 -0500

    Made initial Logic

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int currentOp(int total, int a, char op ) {
+
+    if (op == '+') return total + a;
+
+    if (op == '-') return total - a;
+
+    if (op == '*') return total * a;
+
+    return total;
+
+}
+
+
+int calc(const vector<string>& totalGrid, int r, int c, char op, int total) {
+
+    if(r < 0 || r >= totalGrid.size() || c >= totalGrid[r].size()) return total;
+
+    char currentChar = totalGrid[r][c];
+
+    if(isdigit(currentChar)) {
+
+        total = currentOp(total, currentChar - '0', op);
+    }
+
+    else if(currentChar == '+' || currentChar == '-' || currentChar == '*') {
+
+        op = currentChar;
+
+    }
+
+    else if(currentChar == '^') {
+
+        total = calc(totalGrid, r - 1, c, op, total);
+    }
+
+    else if(currentChar == 'v') {
+
+        total = calc(totalGrid, r + 1, c, op, total);
+    }
+    
+    return calc(totalGrid, r, c + 1, op, total);
+}
+
+
+int initialCalc(const vector<string>& totalGrid) {
+
+    return calc(totalGrid, 0, 0, '+', 0);
+
+}
+
+
+int main() {
+
+    vector<string> totalGrid = {
+        "3+2*2v",
+        " ^   v",
+        "1-5*3"
+    };
+
+    cout << initialCalc(totalGrid) << endl;
+
+    return 0;
+}
